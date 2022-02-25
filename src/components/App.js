3ab4725885350a5954a7b537f3a3f79ab6f6c270
AkshitaDadheech <dadheechakshita@gmail.com>
2022-02-25 17:09:04 +0530

    updated code1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
+import { signUpFormValidation } from '../utils/validation';
 
 
 const App = () => {
+  const[formData,setForm]=useState({
+    "email":'',
+    "password":'',
+    "name":'',
+    "consent": ''
+  })
+  const[err,setError]=useState({
+    name:'',
+    password:'',
+    email:''
+  })
+  function handleNameChange(event){
+    setForm({...formData, name: event.target.value})
+  }
+  function handleEmailChange(event){
+    setForm({...formData, email: event.target.value})
+  }
+  function handlePasswordChange(event){
+    setForm({...formData, password: event.target.value})
+  }
+  function handleCheckboxChange(event){
+    console.log(event.target.value);
+    setForm({...formData, consent: event.target.value})
+  }
+  function handleSubmit(event){
+    event.preventDefault()
+    const obj={email:formData.email,
+      password:formData.password,
+      name:formData.name}
+    const err=signUpFormValidation(obj)
+    setError(err)
+  }
+  /*const handleRequest = (event)=>{
+    event.preventDefault()
+    const obj={email:email,
+      password:password,
+      name:name}
+    const err=signUpFormValidation(obj)
+    setError(err)
+  } */
   
   return (
-
+      <div>
+        <label>Name: </label>
+        <input id='name' type='text' value={formData.name} placeholder='enter name of user' onChange={handleNameChange}/><br/>
+        <div>{err.name}</div><br/>
+        <label>Email: </label>
+        <input id='email' type='text' value={formData.email} placeholder='enter email of user' onChange={handleEmailChange}/><br/>
+        <div>{err.email}</div><br/>
+        <label>Password: </label>
+        <input id='password' type='password' value={formData.password} placeholder='enter password of user' onChange={handlePasswordChange}/><br/>
+        <div>{err.password}</div><br/>
+        <label>Consent: </label>
+        <input id='consent' type='checkbox' onChange={handleCheckboxChange}/><br/>
+        
+        <button onClick={handleSubmit}>Sign Up</button>
+        
+      </div>
   )
 }
 
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,4 @@
-const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; 
+const pattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/"; 
 
 function isEmailAddress(str) {
 
